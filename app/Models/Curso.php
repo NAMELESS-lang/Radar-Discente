f8042ae668cfc6f31b8153cec0668f45e06eaa6f
matheus <matheusfiedlercolla@gmail.com>
2026-09-21 09:57:17 -0300

    Corrigido o 1:N entre curso e turma e curso e instituicao

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -4,18 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
 
 class Curso extends Model
 {
     // Representa 1:N com Instituicao. Vários cursos pertencem a uma Instituicao
-    public function Instituicao(): HasOne
+    public function Instituicao(): belongsTo
     {
-        return $this->hasOne(Instituicao::class);
+        return $this->belongsTo(Instituicao::class);
     }
 
 
-     // Representa a relação 1:N. Uma instituição possui vários cursos
+     // Representa a relação 1:N. Um curso possui várias turmas
     public function Turmas(): HasMany
     {
         return $this->hasMany(Turma::class);
--- app/Models/Turma.php
+++ app/Models/Turma.php
@@ -3,12 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
 class Turma extends Model
 {
     // Define o relacionamento 1:N. Várias turmas pertencem a um curso
-    public function Curso(): HasOne
+    public function Curso(): belongsTo
     {
-        return $this->hasOne(Curso::class);
+        return $this->belongsTo(Curso::class);
     }
 }
