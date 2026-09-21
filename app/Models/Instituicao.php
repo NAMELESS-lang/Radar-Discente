ccbe8f2a784e7710e28f207b3d8d2082c3915a59
matheus <matheusfiedlercolla@gmail.com>
2026-09-21 09:46:41 -0300

    Adicionado model Cursos e feito 1:N entre Instituicao e Curso

--- app/Models/Instituicao.php
+++ app/Models/Instituicao.php
@@ -7,8 +7,16 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Instituicao extends Model
 {
+
+    // Representa a relação 1:N. Uma instituição possui vários cursos
+    public function Cursos(): HasMany
+    {
+        return $this->hasMany(Curso::class);
+    }
+
+
     // Representa a relação 1:N. Uma instituição possui vários usuários
-    public function comments(): HasMany
+    public function Usuarios(): HasMany
     {
         return $this->hasMany(Usuario::class);
     }
