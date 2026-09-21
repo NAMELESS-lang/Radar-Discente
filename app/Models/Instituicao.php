bfbdf4320df9b38126d6189ff0e07a3564cbd1ac
matheus <matheusfiedlercolla@gmail.com>
2026-09-21 09:39:59 -0300

    Adicionado model Funcoes, Instituicao, Usuario e o relacionamento N:N entre Usuario e Funcao, assim como 1:N entre Instituicao e Usuario

--- app/Models/Instituicao.php
+++ app/Models/Instituicao.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Instituicao extends Model
 {
-    //
+    // Representa a relação 1:N. Uma instituição possui vários usuários
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Usuario::class);
+    }
 }
---
+++ app/Models/Usuario.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\belongsToMany;
+
+class Usuario extends Model
+{
+
+    // Representa o relacionamento N:N, vários usuários exercem várias funções
+    // A tabela do relacionamento N:N é usuario_funcoes
+    public function Funcoes(): belongsToMany
+    {
+        return $this->belongsToMany(Funcoes::class, 'usuario_funcoes');
+    }
+}
