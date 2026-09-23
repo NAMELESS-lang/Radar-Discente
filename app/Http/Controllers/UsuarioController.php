ec7e2f30cc020b4860425be7cafff78497f9afb8
matheus <matheusfiedlercolla@gmail.com>
2026-09-23 16:49:28 -0300

    Adicionado métodos principais UsuarioController

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Usuario;
 
 class UsuarioController extends Controller
 {
-    //
+    public function cadarstrarUsuario(Request $request){
+    }
+
+    public function atualizarUsuario(Request $request){
+    }
+
+    public function deletarUsuario(Request $request){
+    }
+
+    public function buscarId(int $id){
+    }
+
+    public function buscarNome(string $nome){
+    }
+
+    public function buscarMatricula(string $matricula){
+    }
 }
